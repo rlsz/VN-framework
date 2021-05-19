f999738f6b47dddf36f6bf841e4a4ce04a3582c0
李森森 <lisensen2333@icloud.com>
2021-05-19 11:37:51 +0800

    save

--- src/public/platform/index.js
+++ src/public/platform/index.js
@@ -1,8 +1,7 @@
 import platformDirective from "./platform.directive";
-import {PlatformService} from "./platform.service";
 export * from './platform'
 export * from './platform.service'
 
 export default function(Vue) {
-    Vue.directive('platform', platformDirective(PlatformService.instance.platform))
+    Vue.use(platformDirective)
 }
--- src/public/platform/platform.directive.js
+++ src/public/platform/platform.directive.js
@@ -1,7 +1,27 @@
-export default function (platform) {
-    return {
-        bind: function (el, binding, vnode) {
-            el.classList.add(platform)
-        }
+import {Directive, DirectiveContext} from "../base/directives/directive-base";
+import {PlatformService} from "./platform.service";
+import {Platform} from "./platform";
+
+class VPlatformDirective extends DirectiveContext {
+    bind() {
+        super.bind(...arguments)
+        this.refresh(...arguments)
     }
+    update() {
+        super.update(...arguments);
+        this.refresh(...arguments)
+    }
+
+    refresh(el, binding, vnode) {
+        const componentInstance = vnode.componentInstance || vnode.context
+        const ps = componentInstance.$injector.get(PlatformService)
+        el.classList.remove(Platform.pc)
+        el.classList.remove(Platform.mobile)
+        el.classList.add(ps.platform)
+    }
+}
+
+
+export default function () {
+    new Directive('platform').register(VPlatformDirective)
 }
