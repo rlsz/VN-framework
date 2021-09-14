72e3e47967d74e38bb73e410ea5fa9c48d841c88
李森森 <lisensen2333@icloud.com>
2021-09-14 20:57:54 +0800

    save

--- src/public/adapter/default/dialog-item.vue
+++ src/public/adapter/default/dialog-item.vue
@@ -1,186 +1,201 @@
 <script>
-import {Dialog, Model, Position} from "../../dialogs/dialog";
-import {PlatformService} from '../../platform/platform.service'
-import {Platform} from '../../platform/platform'
-import {getScrollParent} from '../../base/utils'
+import { Dialog, Model, Position } from "../../dialogs/dialog";
+import { PlatformService } from "../../platform/platform.service";
+import { Platform } from "../../platform/platform";
+import { getScrollParent } from "../../base/utils";
 import {ConfigService} from "../../config.service";
 
 function round(num) {
-  if(num >= 0) {
-    return Math.round(num)
+  if (num >= 0) {
+    return Math.round(num);
   } else {
-    return -Math.round(-num)
+    return -Math.round(-num);
   }
 }
 
 function calcTransform(anchorEl, targetEl, position) {
   if (!anchorEl || !targetEl) {
-    return null
+    return null;
   }
-  const anchor = anchorEl.getBoundingClientRect() // a, ax, ay, aw, ah
-  const self = targetEl.getBoundingClientRect() // s, sx, sy, sw, sh
+  const anchor = anchorEl.getBoundingClientRect(); // a, ax, ay, aw, ah
+  const self = targetEl.getBoundingClientRect(); // s, sx, sy, sw, sh
   if (!anchor || !self) {
-    return null
+    return null;
   }
-  let translateX = 0
-  let translateY = 0
+  let translateX = 0;
+  let translateY = 0;
   if ([Position.bottom, Position.bottomStrict].indexOf(position) >= 0) {
     // x = -((sx + sw/2) - (ax + aw/2))
-    translateX = anchor.x + anchor.width / 2 - self.x - self.width / 2
+    translateX = anchor.x + anchor.width / 2 - self.x - self.width / 2;
     // y = -(sy - ay - ah)
-    translateY = anchor.y + anchor.height - self.y
+    translateY = anchor.y + anchor.height - self.y;
   }
   if (position === Position.top) {
     // x = -((sx + sw/2) - (ax + aw/2))
-    translateX = anchor.x + anchor.width / 2 - self.x - self.width / 2
+    translateX = anchor.x + anchor.width / 2 - self.x - self.width / 2;
     // y = -(sy + sh - ay)
-    translateY = anchor.y - self.y - self.height
+    translateY = anchor.y - self.y - self.height;
   }
   if (position === Position.left) {
     // x = - (sx + sw - ax)
-    translateX = anchor.x - self.x - self.width
+    translateX = anchor.x - self.x - self.width;
     // y = - ((sy + sh/2) - (ay + ah/2))
-    translateY = anchor.y + anchor.height / 2 - self.y - self.height / 2
+    translateY = anchor.y + anchor.height / 2 - self.y - self.height / 2;
   }
   if (position === Position.right) {
     // x = - (sx - ax - aw)
-    translateX = anchor.x + anchor.width - self.x
+    translateX = anchor.x + anchor.width - self.x;
     // y = - ((sy + sh/2) - (ay + ah/2))
-    translateY = anchor.y + anchor.height / 2 - self.y - self.height / 2
+    translateY = anchor.y + anchor.height / 2 - self.y - self.height / 2;
   }
-  const body = document.body.getBoundingClientRect()
-  translateY = Math.min(body.height - self.height - self.y, translateY)
-  translateY = Math.max(-self.y, translateY)
-  translateX = Math.min(body.width - self.width - self.x, translateX)
-  translateX = Math.max(-self.x, translateX)
+  const body = document.body.getBoundingClientRect();
+  translateY = Math.min(body.height - self.height - self.y, translateY);
+  translateY = Math.max(-self.y, translateY);
+  translateX = Math.min(body.width - self.width - self.x, translateX);
+  translateX = Math.max(-self.x, translateX);
 
-  let maxHeight = null
+  let maxHeight = null;
   if (position === Position.bottomStrict) {
-    translateY = Math.max(anchor.y + anchor.height - self.y, translateY)
-    maxHeight = body.height - anchor.y - anchor.height - 4
+    translateY = Math.max(anchor.y + anchor.height - self.y, translateY);
+    maxHeight = body.height - anchor.y - anchor.height - 4;
   }
 
-  translateX = round(translateX)
-  translateY = round(translateY)
-  return {x: translateX, y: translateY, maxHeight}
+  translateX = round(translateX);
+  translateY = round(translateY);
+  return { x: translateX, y: translateY, maxHeight };
 }
 
 export default {
   name: "dialog-item",
-  props: ['options'],
+  props: ["options"],
   di: {
-    providers: [{
-      provide: Dialog,
-      useFactory() {
-        return this.$options.propsData.options.instance
-      }
-    }],
+    providers: [
+      {
+        provide: Dialog,
+        useFactory() {
+          return this.$options.propsData.options.instance;
+        },
+      },
+    ],
     inject: {
       dialog: Dialog,
       configService: ConfigService
-    }
+    },
   },
   data() {
     return {
-      transform: {x: 0, y: 0},
-      transformPointer: {x: 0, y: 0},
+      transform: { x: 0, y: 0 },
+      transformPointer: { x: 0, y: 0 },
       listener: null,
       maxHeight: null,
-      hide: false
-    }
+      hide: false,
+    };
   },
   computed: {
     model() {
-      const config = this.options.instance.config
+      const config = this.options.instance.config;
       if (config && config.anchor) {
-        return Model.positionByAnchor
+        return Model.positionByAnchor;
       }
       if (config && config.model) {
-        return config.model
+        return config.model;
       }
-      return PlatformService.instance.platform === Platform.pc ? Model.float : Model.fillAvailable
+      return PlatformService.instance.platform === Platform.pc
+          ? Model.float
+          : Model.fillAvailable;
     },
     position() {
-      const config = this.options.instance.config
+      const config = this.options.instance.config;
       if (config && config.position) {
-        return config.position
+        return config.position;
       }
-      return Position.bottom
+      return Position.bottom;
     },
     enableOverlayClose() {
-      if (!this.options.instance.config) {
-        return !this.configService?.dialogDisableClose;
+      if (this.options.instance.config) {
+        if (this.options.instance.config["close-on-click-overlay"] !== undefined) {
+          return this.options.instance.config["close-on-click-overlay"];
+        }
+        if (this.options.instance.config.disableClose !== undefined) {
+          return !this.options.instance.config.disableClose;
+        }
       }
-      if (
-          this.options.instance.config["close-on-click-overlay"] !== undefined
-      ) {
-        return this.options.instance.config["close-on-click-overlay"];
-      }
-      if (this.options.instance.config.disableClose !== undefined) {
-        return !this.options.instance.config.disableClose;
-      }
-      return !this.configService?.dialogDisableClose;
+      return this.model === Model.positionByAnchor || !this.configService?.dialogDisableClose;
     }
   },
   // https://vuejs.org/v2/guide/render-function.html#createElement-Arguments
   render(h) {
-    let transform = '', transformPointer = ''
+    let transform = "",
+        transformPointer = "";
     if (this.model === Model.positionByAnchor) {
-      transform = `translateX(${this.transform.x}px) translateY(${this.transform.y}px)`
-      transformPointer = `translateX(${this.transformPointer.x}px) translateY(${this.transformPointer.y}px)`
+      transform = `translateX(${this.transform.x}px) translateY(${this.transform.y}px)`;
+      transformPointer = `translateX(${this.transformPointer.x}px) translateY(${this.transformPointer.y}px)`;
     }
-    return h('div', {
-      class: {
-        'dialog-container': true,
-        [this.model]: true
-      },
-      on: {
-        click: this.onOverlayClick
-      },
-      style: {
-        'opacity': this.hide ? '0' : '1'
-      }
-    }, [
-      h('div', {
-        class: 'dialog-panel',
-        ref: 'dialogPanel',
-        style: {
-          transform,
-          maxHeight: this.maxHeight ? this.maxHeight + 'px' : null
-        }
-      }, [h(this.options.vueComponent)]),
-      transformPointer ? h('i', {
-        class: {
-          'dialog-anchor-pointer': true,
-          [this.position]: true
+    return h(
+        "div",
+        {
+          class: {
+            "dialog-container": true,
+            [this.model]: true,
+          },
+          on: {
+            click: this.onOverlayClick,
+          },
+          style: {
+            opacity: this.hide ? "0" : "1",
+          },
         },
-        ref: 'dialogAnchorPointer',
-        style: {
-          transform: transformPointer
-        }
-      }) : null
-    ])
+        [
+          h(
+              "div",
+              {
+                class: "dialog-panel",
+                ref: "dialogPanel",
+                style: {
+                  transform,
+                  maxHeight: this.maxHeight ? this.maxHeight + "px" : null,
+                },
+              },
+              [h(this.options.vueComponent)]
+          ),
+          transformPointer
+              ? h("i", {
+                class: {
+                  "dialog-anchor-pointer": true,
+                  [this.position]: true,
+                },
+                ref: "dialogAnchorPointer",
+                style: {
+                  transform: transformPointer,
+                },
+              })
+              : null,
+        ]
+    );
   },
   mounted() {
     this.options.instance.setOpen(this);
-    this.fixPositionByAnchor()
+    this.fixPositionByAnchor();
   },
   updated() {
-    this.fixPositionByAnchor()
+    this.fixPositionByAnchor();
   },
   destroyed() {
     if (this.listener) {
-      this.listener.target.removeEventListener('scroll', this.listener.onScroll)
+      this.listener.target.removeEventListener(
+          "scroll",
+          this.listener.onScroll
+      );
       setTimeout(() => {
-        document.body.removeEventListener('click', this.onBodyClick);
-      }, 0)
+        document.body.removeEventListener("click", this.onBodyClick);
+      }, 0);
     }
   },
   methods: {
     onOverlayClick(event) {
       if (event.target === event.currentTarget) {
         if (this.enableOverlayClose) {
-          this.options.instance.close()
+          this.options.instance.close();
         }
       }
     },
@@ -189,68 +204,73 @@ export default {
           this.enableOverlayClose &&
           event.target &&
           !this.$el.contains(event.target) &&
-          document.body.querySelector('#app').contains(event.target)
+          document.body.querySelector("#app").contains(event.target)
       ) {
-        this.options.instance.close()
+        this.options.instance.close();
       }
     },
     fixPositionByAnchor() {
       if (this.model !== Model.positionByAnchor) {
-        return
+        return;
       }
       if (!this.options.instance.config?.anchor) {
-        this.transform = {x: 0, y: 0}
-        return
+        this.transform = { x: 0, y: 0 };
+        return;
       }
       if (!this.listener) {
-        const target = getScrollParent(this.options.instance.config.anchor)
+        const target = getScrollParent(this.options.instance.config.anchor);
         this.listener = {
           target: target,
           onScroll: this.onAnchorMove.bind(this),
-        }
-        target.addEventListener('scroll', this.listener.onScroll)
+        };
+        target.addEventListener("scroll", this.listener.onScroll);
         setTimeout(() => {
-          document.body.addEventListener('click', this.onBodyClick, false);
-        }, 0)
+          document.body.addEventListener("click", this.onBodyClick, false);
+        }, 0);
       }
 
       const transform = calcTransform(
           this.options.instance.config.anchor,
           this.$refs.dialogPanel,
           this.position
-      )
-      if(!transform) {
-        this.transform = {x: 0, y: 0}
-        return
+      );
+      if (!transform) {
+        this.transform = { x: 0, y: 0 };
+        return;
       }
-      if(transform.maxHeight !== this.maxHeight) {
-        this.maxHeight = transform.maxHeight
+      if (transform.maxHeight !== this.maxHeight) {
+        this.maxHeight = transform.maxHeight;
       }
       if (Math.abs(transform.x) > 1 || Math.abs(transform.y) > 1) {
-        const {x, y} = this.transform
-        this.transform = {x: x + transform.x, y: y + transform.y}
+        const { x, y } = this.transform;
+        this.transform = { x: x + transform.x, y: y + transform.y };
       }
 
       const transformPointer = calcTransform(
           this.options.instance.config.anchor,
           this.$refs.dialogAnchorPointer,
           this.position
-      )
-      if(!transformPointer) {
-        this.transformPointer = {x: 0, y: 0}
-        return
+      );
+      if (!transformPointer) {
+        this.transformPointer = { x: 0, y: 0 };
+        return;
       }
-      if (Math.abs(transformPointer.x) > 1 || Math.abs(transformPointer.y) > 1) {
-        const {x, y} = this.transformPointer
-        this.transformPointer = {x: x + transformPointer.x, y: y + transformPointer.y}
+      if (
+          Math.abs(transformPointer.x) > 1 ||
+          Math.abs(transformPointer.y) > 1
+      ) {
+        const { x, y } = this.transformPointer;
+        this.transformPointer = {
+          x: x + transformPointer.x,
+          y: y + transformPointer.y,
+        };
       }
     },
     onAnchorMove() {
-      this.fixPositionByAnchor()
+      this.fixPositionByAnchor();
     },
-
-  }
-}
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -364,7 +384,8 @@ i.dialog-anchor-pointer {
     transform: rotate(45deg) translateY(-2px);
   }
 
-  &.bottom:after, &.bottomStrict:after {
+  &.bottom:after,
+  &.bottomStrict:after {
     margin-top: -3px;
     transform: rotate(-135deg) translateY(-2px);
   }
