2f722c2df69ba1d167ea0881be51a63cb31ce90f
李森森 <lisensen2333@icloud.com>
2021-09-17 15:56:10 +0800

    save

--- src/public/base/directives/v-file-preview/file-preview.vue
+++ src/public/base/directives/v-file-preview/file-preview.vue
@@ -4,7 +4,7 @@
     <video v-else-if="dialog.config.type === FileType.video" controls :src="dialog.config.src">
       <p>Your browser doesn't support HTML5 video. Here is a <a :href="dialog.config.src" target="_blank">link to the video</a> instead.</p>
     </video>
-    <span v-else>{{dialog.config.src}}</span>
+    <iframe v-else :src="dialog.config.src" frameborder="0" class="iframe"></iframe>
   </div>
 </template>
 
@@ -37,5 +37,8 @@ export default {
 video, img {
   max-width: 100%;
 }
-
+iframe {
+  min-width: calc(100vw - 100px);
+  min-height: calc(100vh - 100px);
+}
 </style>
