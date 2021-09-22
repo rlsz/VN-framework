3d0ffa2df371b1a1ff944794e9d705bceb8eb904
李森森 <lisensen2333@icloud.com>
2021-09-22 16:50:13 +0800

    save

--- src/public/form/el-validators.js
+++ src/public/form/el-validators.js
@@ -88,7 +88,7 @@ export class ElFormValidators {
                 }
                 return callback()
             },
-            trigger: 'blur'
+            trigger: ['blur', 'change']
         }
     }
 
