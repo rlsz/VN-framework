88d6979361fbe6e838d997c5e334929d648f885e
李森森 <lisensen2333@icloud.com>
2021-10-08 13:37:28 +0800

    save

--- src/app/test/components/MovieService.js
+++ src/app/test/components/MovieService.js
@@ -1,21 +1,34 @@
-import {throttle, ToggleFullScreen} from "@/public/base";
+import {throttle, timer, ToggleFullScreen} from "@/public/base";
+import {LoggerService} from "@/public/logger";
 
 export class MovieService {
     src = null
     resolve
     vm
+    injector
+
+    get ls() {
+        return this.injector.get(LoggerService)
+    }
+
+    constructor(injector) {
+        this.injector = injector
+    }
 
     get videoRef() {
         return this.vm.$refs.video
     }
     play(file) {
         this.src = URL.createObjectURL(file)
-        return new Promise(r => {
-            this.resolve = r
+        return ToggleFullScreen(this.videoRef, true).then(() => {
+            return new Promise(r => {
+                this.resolve = r
+            })
         })
     }
     end() {
-        this.resolve('ended')
+        this.ls.info('video end...')
+        timer(2000).then(() => this.resolve('ended'))
     }
 
     diCreated(vm) {
@@ -23,19 +36,17 @@ export class MovieService {
     }
 
     multiPlay(children) {
-        ToggleFullScreen(this.videoRef, true).then(() => {
-            const playOne = throttle(comp => {
-                console.log('playing:', comp.value.name)
-                return comp.play().then(res => {
-                    if(res !== 'ended') {
-                        throw new Error('video autoplay canceled')
-                    }
-                })
-            }, 1)
-            const videos = Array.from(children)
-                .map(c => c.__vue__)
-                .filter(c => c.isVideo)
-            videos.forEach(c => playOne(c))
-        })
+        const playOne = throttle(comp => {
+            console.log('playing:', comp.value.name)
+            return comp.play().then(res => {
+                if(res !== 'ended') {
+                    throw new Error('video autoplay canceled')
+                }
+            })
+        }, 1)
+        const videos = Array.from(children)
+            .map(c => c.__vue__)
+            .filter(c => c.isVideo)
+        videos.forEach(c => playOne(c))
     }
 }
