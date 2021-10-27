8df5a33959b19990fbf2cc044103c1f68a62ec18
李森森 <lisensen2333@icloud.com>
2021-10-27 14:20:49 +0800

    document update

--- src/public/base/utils.js
+++ src/public/base/utils.js
@@ -808,7 +808,7 @@ export const DownloadByUrl = throttle(function (url, fileName) {
         link.download = fileName // doesn't work for cross-origin requests
     }
     link.click();
-    return timer(1000) // delay 1s for next download (if not, last download will be canceled in chrome)
+    return timer(1000) // delay 1s for next download (if not, all previous download tasks will be canceled in chrome)
 }, 1)
 
 export function DownloadByBlob(blob, fileName) {
