fc0e9db284851691077e26de6d4245d65536c56b
李森森 <lisensen2333@icloud.com>
2021-12-30 10:04:53 +0800

    save

--- src/public/form/form-model.js
+++ src/public/form/form-model.js
@@ -1,6 +1,6 @@
 export const FORM_MODEL = Symbol('form-model')
 export const FormModel = {
-    edit: 1,
-    detail: 2,
-    disabled: 3
+    edit: 'edit',
+    detail: 'detail',
+    disabled: 'disabled'
 }
