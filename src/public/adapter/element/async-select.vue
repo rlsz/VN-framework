7009eac6ace43d484201acdcb6c4615364d980f3
李森森 <lisensen2333@icloud.com>
2021-12-31 15:56:15 +0800

    save

--- src/public/adapter/element/async-select.vue
+++ src/public/adapter/element/async-select.vue
@@ -1,11 +1,14 @@
 <template>
+  <span v-if="refreshFlag"></span>
   <el-select
       :loading="loading"
-      v-bind="{...$props, ...$attrs, asyncOptions: undefined, valueKey: idKey || valueKey}"
+      v-bind="{filterable: !!remoteMethod, remote: !!remoteMethod, 'reserve-keyword': !!remoteMethod, ...$props, ...$attrs, asyncOptions: undefined, valueKey: idKey || valueKey}"
       v-on="{...$listeners}"
-      v-if="editable"
+      v-el-select-modify
+      :remote-method="customRemoteMethod"
+      v-else-if="editable"
   >
-    <slot v-for="(item,index) in options" v-bind:value="item" v-bind:index="index">
+    <slot v-for="(item,index) in allOptions" v-bind:value="item" v-bind:index="index">
       <el-option
           :key="getId(item)"
           :label="getLabel(item)"
@@ -32,10 +35,11 @@
 <script>
 import {FORM_MODEL, FormModel} from "../../form/form-model";
 import {Optional} from "../../di.service";
+import {Distinct} from "../../base/utils";
 
 export default {
   name: "async-select",
-  props: ['asyncOptions', 'value', 'valueKey', 'labelKey', 'idKey', 'multiple'], // asyncOptions: Promise instance
+  props: ['asyncOptions', 'value', 'valueKey', 'labelKey', 'idKey', 'multiple', 'remoteMethod'], // asyncOptions: Promise instance
   di: {
     inject: {
       formModel: Optional(FORM_MODEL)
@@ -44,7 +48,8 @@ export default {
   data() {
     return {
       options: [],
-      loading: false
+      loading: false,
+      refreshFlag: false
     }
   },
   computed: {
@@ -65,14 +70,37 @@ export default {
         return true
       }
       return false
+    },
+    valueOptions() {
+      if (this.multiple === false) {
+        return this.value ? [this.value] : []
+      } else {
+        return this.value
+      }
+    },
+    allOptions() {
+      if (this.idKey || this.valueKey) {
+        return Distinct(
+            [].concat(this.options || [], this.valueOptions || []),
+            (a, b) => this.getId(a) === this.getId(b)
+        )
+      } else {
+        return this.options
+      }
     }
   },
   watch: {
     asyncOptions(val) {
       this.getData()
+    },
+    value(newVal, oldVal) {
+      if(newVal !== oldVal) {
+        this.refresh()
+      }
     }
   },
   created() {
+    this.options = this.valueOptions
     this.getData()
   },
   methods: {
@@ -106,6 +134,40 @@ export default {
       } else {
         return val
       }
+    },
+    customRemoteMethod(keyword) {
+      this.loading = true
+      Promise.resolve(this.remoteMethod(...arguments)).then(data => {
+        this.options = data
+      }).finally(() => {
+        this.loading = false
+      })
+    },
+    /**
+     * el-select在value发生变化时getOption方法得到的newOption的currentLabel为空，导致el-tag显示错误，
+     * 通过refresh强制销毁重建可以恢复正常
+     *
+     * 这个修改会导致的变化：
+     * 旧：
+     *  1、多选输入框获取焦点后不会出现下拉框
+     *  2、输入关键词后触发远程搜索，然后出现下拉框，失去焦点后下拉框消失
+     *  3、删除关键词不会导致下拉框消失
+     *  4、选中下拉框选项后下拉框和输入内容保留，可以继续选择其他选项
+     *  5、从外部修改v-model绑定的属性时可能导致el-tag显示错误
+     * 新：
+     *  1、同上
+     *  2、同上
+     *  3、同上
+     *  4、选中下拉框选项后el-select会被重置，输入内容消失、下拉框收起，但下拉框选项内容保留
+     *  5、修复el-tag显示错误的问题
+     *
+     *  已与产品确认过，新的交互方式要优于旧的交互方式，所有多选输入组件可以使用本组件代替
+     */
+    refresh() {
+      this.refreshFlag = true
+      this.$nextTick(() => {
+        this.refreshFlag = false
+      })
     }
   }
 }
